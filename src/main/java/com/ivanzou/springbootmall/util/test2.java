88304d6adaa48539b85b2f5338b7cd1379313be3
tkb0005053 <tkb5053ivan@gmail.com>
2023-11-21 11:37:46 +0800

    create main function in test2

--- src/main/java/com/ivanzou/springbootmall/util/test2.java
+++ src/main/java/com/ivanzou/springbootmall/util/test2.java
@@ -1,7 +1,10 @@
 package com.ivanzou.springbootmall.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class test2 {
     public static void main(String[] args) {
-
+        Map<String, Object> map = new HashMap<>();
     }
 }
